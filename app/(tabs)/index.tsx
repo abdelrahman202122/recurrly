8dae998606480e3716302ac926c952e8b7fbd1b8
Abdelrahman <abdoezzat098@gmail.com>
2026-03-29 20:41:57 +0200

    feat: project setup, nativewind, routing, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,39 @@
-import { Text, View } from "react-native";
-import {Link} from "expo-router";
+import { Link } from 'expo-router';
+
+import { Text } from 'react-native';
+
+import { styled } from 'nativewind';
+import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
-      <Link href='/onboarding' className="text-xl font-bold text-primary">Onboarding</Link>
-      <Link href='/(auth)/sign-in' className="text-xl font-bold text-primary">Sign In</Link>
-      <Link href='/(auth)/sign-up' className="text-xl font-bold text-primary">Sign Up</Link>
-      <Link href='/subscriptions/spotify' className="text-xl font-bold text-primary">Spotify Subscription</Link>
-      <Link href={{pathname: '/subscriptions/[id]', params: {id:"claude"}}} className="text-xl font-bold text-primary">Claude Subscription</Link>
-    </View>
+      <Link href="/onboarding" className="text-xl font-bold text-primary">
+        Onboarding
+      </Link>
+      <Link href="/(auth)/sign-in" className="text-xl font-bold text-primary">
+        Sign In
+      </Link>
+      <Link href="/(auth)/sign-up" className="text-xl font-bold text-primary">
+        Sign Up
+      </Link>
+      <Link
+        href={{ pathname: '/subscriptions/[id]', params: { id: 'spotify' } }}
+        className="text-xl font-bold text-primary"
+      >
+        Spotify Subscription
+      </Link>
+      <Link
+        href={{ pathname: '/subscriptions/[id]', params: { id: 'claude' } }}
+        className="text-xl font-bold text-primary"
+      >
+        Claude Subscription
+      </Link>
+    </SafeAreaView>
   );
 }
